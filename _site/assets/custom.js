a16ccf1f631d5bd1021d175238ec3243d856a27c
Nikhil Thota <nt92@ufl.edu>
2017-04-18 16:56:14 -0400

    added bar and basic stuff

---
+++ _site/assets/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.right.menu.open').on("click",function(e){
+    e.preventDefault();
+     $('.ui.vertical.menu').toggle();
+   });
+   $('.ui.dropdown').dropdown();
+ });
--- updateSite.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MINPARAMS=1
-
-jekyll build
-git add .
-git commit -m "$1"
-git push
-# git push origin production
