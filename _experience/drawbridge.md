8647bf21eb079830ea1b2fa72b1a162013bf7852
Nikhil Thota <nt92@ufl.edu>
2017-07-22 17:27:14 -0700

    fixed email form

--- _experience/drawbridge.md
+++ _experience/drawbridge.md
@@ -6,6 +6,6 @@ start: May 2017
 end: August 2017
 image: drawbridge.png
 location: San Francisco, California
-brief: Incoming Intern as a Software Engineering Intern on the Data Science Team
+brief: Developing systems to prevent click fraud and improving the granularity of the bidding algorithms
 date: 2017-04-15 00:00:00
 ---
--- _site/assets/custom.js
+++ _site/assets/custom.js
@@ -4,41 +4,43 @@ $(document).ready(function(){
      $('.ui.vertical.menu').toggle();
    });
    $('.ui.dropdown').dropdown();
+
+   $('#contact-form').submit(function(e) {
+     console.log('yue');
+   	e.preventDefault();
+   	var c_name = $('#c_name').val();
+   	var c_email = $('#c_email').val();
+   	var c_message = $('#c_message ').val();
+   	var response = $('#contact-form .ajax-response');
+   	$('#response-text').html("Thank you!");
+
+   	var formData = {
+   		'name'       : c_name,
+   		'email'      : c_email,
+   		'message'    : c_message
+   	};
+
+   	if (( c_name== '' || c_email == '' || c_message == '')) {
+   		response.fadeIn(500);
+   		response.html('<i class="fa fa-warning"></i> Please fix the data and try again.');
+   	}
+
+   	else {
+   	   $.ajax({
+   		  type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+   			url         : 'https://formspree.io/nt92@ufl.edu', // the url where we want to POST
+   			data        : formData, // our data object
+   			dataType    : 'json', // what type of data do we expect back from the server
+   			encode      : true,
+   		success		: function(res){
+   			var ret = $.parseJSON(JSON.stringify(res));
+   			response.html(ret.message).fadeIn(500);
+   			}
+   		});
+   	}
+     return false;
+   });
  });
-$('#contact-form').submit(function(e) {
-	e.preventDefault();
-	var c_name = $('#c_name').val();
-	var c_email = $('#c_email').val();
-	var c_message = $('#c_message ').val();
-	var response = $('#contact-form .ajax-response');
-	$('#response-text').html("Thank you!");
-
-	var formData = {
-		'name'       : c_name,
-		'email'      : c_email,
-		'message'    : c_message
-	};
-
-	if (( c_name== '' || c_email == '' || c_message == '') || (!isValidEmailAddress(c_email) )) {
-		response.fadeIn(500);
-		response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
-	}
-
-	else {
-	   $.ajax({
-		  type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-			url         : 'https://formspree.io/nt92@ufl.edu', // the url where we want to POST
-			data        : formData, // our data object
-			dataType    : 'json', // what type of data do we expect back from the server
-			encode      : true,
-		success		: function(res){
-			var ret = $.parseJSON(JSON.stringify(res));
-			response.html(ret.message).fadeIn(500);
-			}
-		});
-	}
-  return false;
-});
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
